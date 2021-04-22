d34868cb2b634ab69c69cb5c0c36bf0584ecd86a
etiennody <etiennody@gmail.com>
2021-04-22 22:39:39 +0200

    added mysql connection

--- admin/settings.py
+++ admin/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "admin.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": env("DATABASE_ENGINE"),
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
     }
 }
 
